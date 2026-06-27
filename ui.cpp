d7745a609653b01dc2f41b7dbf34cee945fe912f
viZPilot <vicenteicc2008@gmail.com>
2026-06-27 16:27:18 -0400

    Replace LineageOS version with VizaOS version

--- ui.cpp
+++ ui.cpp
@@ -80,8 +80,8 @@ RecoveryUI::RecoveryUI()
   char propval[PROPERTY_VALUE_MAX];
   property_get("ro.build.version.release", propval, "(unknown)");
   android_version_ = std::string("Android ") + propval;
-  property_get("ro.lineage.version", propval, "(unknown)");
-  lineage_version_ = std::string("LineageOS ") + propval;
+  property_get("ro.viza.version", propval, "(unknown)");
+  viza_version_ = std::string("VizaOS ") + propval;
 
   pthread_mutex_init(&event_queue_mutex, nullptr);
   pthread_cond_init(&event_queue_cond, nullptr);
